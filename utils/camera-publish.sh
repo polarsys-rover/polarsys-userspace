19a890f5ead1ff14b4e81cb4d936c167f5da8f4a
Simon Marchi <simon.marchi@polymtl.ca>
2016-08-31 16:39:39 -0400

    Add camera-publish.sh script

---
+++ utils/camera-publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function quit() {
+	done=1
+}
+
+trap quit SIGINT
+
+done=0
+
+while [ "$done" -eq 0 ]; do
+	raspistill -w 320 -h 240 -o camera.jpg -t 1 -q 50
+	mosquitto_pub -t /polarsys-rover/front-camera -f camera.jpg
+done
